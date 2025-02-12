2be273c2aafe51e6eac2078295ead12551db0732
666hemanth <hemanthdaxo02@gmail.com>
2025-02-12 15:55:54 +0530

    chore: clean up test files and old configs

--- tests/globalTeardown.js
+++
@@ -1,9 +0,0 @@
-module.exports = async () => {
-  // Clean up any test resources if needed
-  delete process.env.NODE_ENV;
-  delete process.env.JWT_SECRET;
-  delete process.env.MONGODB_URI;
-  delete process.env.PORT;
-  delete process.env.STRIPE_SECRET_KEY;
-  delete process.env.REDIS_URL;
-};
